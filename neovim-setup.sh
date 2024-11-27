46865f621294e62d2892936f8eca8a20d0f8509d
Alessandro Santo <alessandrosanto@Alessandros-MacBook-Air.local>
2024-11-27 13:06:55 +0100

    updated neovim-setup.sh

--- neovim-setup.sh
+++ neovim-setup.sh
@@ -6,36 +6,6 @@ rm -rf /opt/nvim
 tar -C /opt -xzf nvim-linux64.tar.gz
 rm nvim-linux64.tar.gz
 
-# Update package list and install dependencies
-apt-get update && apt-get install -y \
-    clangd \
-    ripgrep \
-    && rm -rf /var/lib/apt/lists/*
-
-# Add NodeSource repository for Node.js
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-
-# Install Node.js and npm packages
-apt-get update && apt-get install -y nodejs
-npm install -g npm@latest
-npm install -g pyright
-
-# Install LuaRocks
-apt-get update && apt-get install -y luarocks
-apt-get clean && rm -rf /var/lib/apt/lists/*
-
-# Configure debugging for Jupyter Notebook
-echo "import os" > $HOME/.pdbrc
-echo "import sys" >> $HOME/.pdbrc
-echo "sys.path.append('/workspace/')" >> $HOME/.pdbrc
-echo "with open('/workspace/debug_helper.py') as file: exec(file.read())" >> $HOME/.pdbrc
-echo "alias save save_state(vars())" >> $HOME/.pdbrc
-
-# Set PYTHONPATH environment variable
-export PYTHONPATH=/workspace/:$PYTHONPATH
-
 # Update PATH
 echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
 
@@ -55,9 +25,20 @@ rm -rf /tmp/config
 curl https://raw.githubusercontent.com/ucl98/config/refs/heads/main/notes/shorcuts.md > /workspace/notes/shortcuts.md
 echo TERM=xterm-256color >> ~/.bashrc
 
-# for neovim linting/suggestions
-pip3 install mypy
-pip3 install ruff
-pip3 install black
-pip3 install debugpy
+# Install wezterm
+curl -fsSL https://apt.fury.io/wez/gpg.key | gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | tee /etc/apt/sources.list.d/wezterm.list
+apt update
+apt install -y wezterm
+
+pip3 install --user matplotlib-backend-wezterm
+pip3 install --user neovim
+
+STARTUP_SCRIPT="/root/.ipython/profile_default/startup/01-matplotlib-wezterm.py"
+
+cat << EOF > "$STARTUP_SCRIPT"
+import matplotlib
+matplotlib.use('module://matplotlib-backend-wezterm')
+import matplotlib.pyplot as plt
+EOF
 
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,7 @@ return {
 		"zsh",
 	},
 	color_scheme = "Catppuccin Mocha",
+	enable_kitty_graphics = true,
 	enable_tab_bar = false,
 	font_size = 15.0,
 	macos_window_background_blur = 5,
