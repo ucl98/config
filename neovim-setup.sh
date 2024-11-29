81d6df80d8dc8b196fcd99b892b2fe2787e6bdc5
Alessandro Santo <alessandrosanto@Alessandros-MacBook-Air.local>
2024-11-29 16:02:18 +0100

    commented the installation of wezterm as that caused some errors (on specific systems)

--- neovim-setup.sh
+++ neovim-setup.sh
@@ -68,10 +68,10 @@ rm -rf /tmp/config
 echo TERM=xterm-256color >> ~/.bashrc
 
 # Install wezterm
-curl -fsSL https://apt.fury.io/wez/gpg.key | gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | tee /etc/apt/sources.list.d/wezterm.list
-apt update
-apt install -y wezterm
+# curl -fsSL https://apt.fury.io/wez/gpg.key | gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
+# echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | tee /etc/apt/sources.list.d/wezterm.list
+# apt update
+# apt install -y wezterm
 
 # Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
